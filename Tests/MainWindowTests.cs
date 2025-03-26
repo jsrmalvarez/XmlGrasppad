74c302e16ea0b680625e16e542c59dc2c87e2f7d
José Ramón Álvarez Rojo <joseramon.alvarez@betterask.erni>
2025-03-26 22:28:33 +0100

    Some interesting tests

--- Tests/MainWindowTests.cs
+++ Tests/MainWindowTests.cs
@@ -26,7 +26,7 @@ public class MainWindowTests
     public void IsMatchingNodeContext_ShouldReturnFalse_ForDifferentParent()
     {
         // Arrange
-        string content = "<book id=\"1\"><title>Introduction to XML</title></book><book id=\"2\"><title>Introduction to XML</title></book>";
+        string content = "<library><book id=\"1\"><title>Introduction to XML</title></book><book id=\"2\"><title>Introduction to XML</title></book></library>";
         string nodeXml = "<title>Introduction to XML</title>";
         int index = content.IndexOf(nodeXml);
         XmlDocument doc = new XmlDocument();
@@ -44,18 +44,21 @@ public class MainWindowTests
     public void IsMatchingNodeContext_ShouldReturnTrue_ForRepeatedValueWithCorrectParent()
     {
         // Arrange
-        string content = "<book id=\"1\"><price>29.99</price></book><book id=\"2\"><price>29.99</price></book>";
+        string content = "<library><book id=\"1\"><price>29.99</price></book><book id=\"2\"><price>29.99</price></book></library>";
         string nodeXml = "<price>29.99</price>";
-        int index = content.IndexOf(nodeXml, content.IndexOf(nodeXml) + 1); // Second occurrence
+        int firstIndex = content.IndexOf(nodeXml); // First occurrence
+        int secondIndex = content.IndexOf(nodeXml, firstIndex + 1); // Second occurrence
         XmlDocument doc = new XmlDocument();
         doc.LoadXml(content);
         XmlNode xmlNode = doc.SelectSingleNode("//book[@id='2']/price");
 
         // Act
-        bool result = XmlNodeMatcher.IsMatchingNodeContext(content, index, nodeXml, xmlNode);
+        bool firstResult = XmlNodeMatcher.IsMatchingNodeContext(content, firstIndex, nodeXml, xmlNode);
+        bool secondResult = XmlNodeMatcher.IsMatchingNodeContext(content, secondIndex, nodeXml, xmlNode);
 
         // Assert
-        Assert.True(result);
+        Assert.False(firstResult); // The first occurrence should not match
+        Assert.True(secondResult); // The second occurrence should match
     }
 
     private bool IsMatchingNodeContext(string content, int index, string nodeXml, XmlNode xmlNode)
